6b20fa78118e9eb7db423b364585a9e8098343a6
Ksenialalala <paksinia@internet.ru>
2026-02-24 12:38:48 +0800

    Проверка условия

--- iftry/Program.cs
+++ iftry/Program.cs
@@ -6,6 +6,10 @@
         {
             double a = 2, b = 4, c = 4.5, d = 1.5;
             bool canPut = (a <= c && b <= c) || (a <= d && b <= d);
+            if (canPut)
+                Console.WriteLine("Можно");
+            else
+                Console.WriteLine("Нельзя");
         }
     }
 }
