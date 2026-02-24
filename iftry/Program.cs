54342c80990cd2b298d179d70d35967ebbcf8b15
Ksenialalala <paksinia@internet.ru>
2026-02-24 12:35:44 +0800

    Задала условие

--- iftry/Program.cs
+++ iftry/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             double a = 2, b = 4, c = 4.5, d = 1.5;
+            bool canPut = (a <= c && b <= c) || (a <= d && b <= d);
         }
     }
 }
