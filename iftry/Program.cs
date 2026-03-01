2df6e31e0ad27c618f14f080b2d7e23917fecf1a
Ksenialalala <paksinia@internet.ru>
2026-03-01 23:48:32 +0800

    Провела декомпозицию

--- iftry/Program.cs
+++ iftry/Program.cs
@@ -1,11 +1,21 @@
 ﻿namespace iftry
 {
+    public class Logic
+    {
+        public static bool CanFit(double a, double b, double c, double d)
+        {
+            return (a <= c && b <= d) || (a <= d && b <= c);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            double a = 2, b = 4, c = 4.5, d = 1.5;
-            bool canPut = (a <= c && b <= d) || (a <= d && b <= c);
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            bool canPut = Logic.CanFit(a, b, c, d);
             if (canPut)
                 Console.WriteLine("Можно");
             else
